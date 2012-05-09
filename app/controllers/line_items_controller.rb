0a404f102a8d0ecae8588be9a693424eaae84d91
Diego Y. K <diegoy@gmail.com>
2012-05-08 22:51:14 -0300

    chapter 11

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -44,9 +44,10 @@ class LineItemsController < ApplicationController
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
  
-    respond_to do |format |
+    respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
+        format.js   { @current_item = @line_item }
         format.json { render json: @line_item, status: :created, location: @line_item }
         session[:counter] = 0
       else
