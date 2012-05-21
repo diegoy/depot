e2c2d90e2182cef9446178d3d527eb0753afcccd
Diego Y. K <diegoy@gmail.com>
2012-05-20 21:18:20 -0300

    Changed the CSS to enlarge the sidebar Partial implementation of checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ActiveRecord::Base
+  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase Order" ]
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
