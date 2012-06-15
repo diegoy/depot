cc9ae3d9961900867757f12ef87d5e84db4fbafd
Diego Y. K <diegoy@gmail.com>
2012-06-15 00:25:05 -0300

    changed environment to add email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,15 @@ Depot::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  Depot::Application.configure do config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = { address: "smtp.gmail.com",
+      port: 587,
+      domain: "domain.of.sender.net", 
+      authentication: "plain",
+      user_name: "",
+      password: "", enable_starttls_auto: true
+    }
+  end
+
 end
