c6f943285583caa77aed067bbc266ee0bf493cc0
Diego Y. K <diegoy@gmail.com>
2012-07-18 23:28:30 -0300

    end of iteration i2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Depot::Application.routes.draw do
+  get "admin" => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    get 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
 
   resources :orders
