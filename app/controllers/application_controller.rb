18bc47c604be6892cd71e8e822113e445cad1b26
Diego Y. K <diegoy@gmail.com>
2012-08-09 22:47:11 -0300

    added basic i18n options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class ApplicationController < ActionController::Base
+  before_filter :set_i18n_locale_from_params
   before_filter :authorize
   protect_from_forgery
 
@@ -20,27 +21,43 @@ class ApplicationController < ActionController::Base
       cart
     end
 
-    protected
+  protected
 
-      def authorize
-        if User.count.zero?
-          redirect_to new_user_path unless session[:new_user]
-          return
-        end
+    def authorize
+      if User.count.zero?
+        redirect_to new_user_path unless session[:new_user]
+        return
+      end
 
-        unless User.find_by_id(session[:user_id])
-          if request.format == Mime::HTML
-            redirect_to login_url, notice: "Please log in"
+      unless User.find_by_id(session[:user_id])
+        if request.format == Mime::HTML
+          redirect_to login_url, notice: "Please log in"
+        elsif
+          if user = authenticate_with_http_basic do |u, p|
+            finded_user = User.find_by_name(u)
+            finded_user.authenticate(p) if finded_user
+          end
+          session[:user_id] = user.id
           elsif
-            if user = authenticate_with_http_basic do |u, p|
-                  finded_user = User.find_by_name(u)
-                  finded_user.authenticate(p) if finded_user
-              end
-              session[:user_id] = user.id
-            elsif
-              render :status => 403, :text => "login failed" and return
-            end
+            render :status => 403, :text => "login failed" and return
           end
         end
       end
+    end
+
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.include?(params[:locale].to_sym)
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] =
+            "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
 end
